1333c2e4bee74b50913cd207f5a7b337d6c826cd
Efren-Asus <eagp80@gmail.com>
2023-04-18 01:35:59 -0500

    Corregido alto de header y sidebar para celulares

---
+++ Clase 2 jueves.js
@@ -0,0 +1,6 @@
+let variable1 = "variable";
+const variable2= "variable2";
+const array1=[1,2,3];
+variable1=100;
+variable1=200;
+console.log(variable1);
---
+++ prueba borrar.js
@@ -0,0 +1,8 @@
+const nombre = "Efren";
+let edad=40;
+let precio= 200;
+
+const series = `el precio es ${precio}`;
+const series2 = `el precio es' + precio`;
+console.log(series);
+console.log(series2);
